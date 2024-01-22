a42580444ab64bf9474bbcc217ddf1274c9af3f1
JulitaJaros <julita.paulina.jaros@gmail.com>
2024-01-22 21:20:47 +0100

    Add data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into unit_of_measure_model(description) values ('Americana');
+insert into unit_of_measure_model(description) values ('Italiana');
+insert into unit_of_measure_model(description) values ('Mexicana');
+insert into unit_of_measure_model(description) values ('Fast food');
+
+insert into category_model(description) values ('Mexicana');
+insert into category_model(description) values ('Fast food');
